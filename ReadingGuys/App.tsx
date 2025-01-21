5651b4c602793e9ee83657bbda4df1d9321926fc
NS-Bae <qw9711@gmail.com>
2025-01-22 08:04:23 +0900

    푸시알림 문제 임시 해결

--- ReadingGuys/App.tsx
+++ ReadingGuys/App.tsx
@@ -4,9 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import messaging from '@react-native-firebase/messaging';
 import { useEffect } from 'react';
-import { Alert } from 'react-native';
+import { PermissionsAndroid, Alert } from 'react-native';
 import axios from 'axios';
-import notifee, { AndroidImportance } from '@notifee/react-native';
+import notifee, { AndroidImportance, AndroidVisibility, EventType } from '@notifee/react-native';
 
 import HomeScreen from './screens/homeScreen';
 import LoginScreen from './screens/loginScreen';
@@ -16,109 +16,118 @@ import { StackParamList } from './types';
 
 const Stack = createNativeStackNavigator<StackParamList>();
 
+async function requestNotificationPermission() {
+  const granted = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
+  );
+
+  if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    console.log('Notification permission granted');
+  } else {
+    console.log('Notification permission denied');
+  }
+}
+
 const App: React.FC = () => {
   useEffect(() => {
-    // 알림 채널 생성
-    const createNotificationChannel = async () => {
-      try
-      {
-        await notifee.createChannel({
-          id: 'default',
-          name: 'Default Channel',
-          importance: AndroidImportance.DEFAULT,  // 중요도 설정
-        });
-      }
-      catch (error)
-      {
-        console.error('알림 채널 생성 오류', error);
-      }
-    };
+    requestNotificationPermission();
     createNotificationChannel();
-
-    //FCM기기토큰 설정
-    const fetchDeviceToken = async () => {
-      try
-      {
-        const deviceToken = await messaging().getToken();
-        console.log('FCM Token : ', deviceToken);
-
-        await sendTokenToServer(deviceToken);
-      }
-      catch(error)
-      {
-        console.log('FCM 토믄 생성 오류', error);
-      }
-    };
-    //FCM푸시알림권한
-    const requestPermission = async () => {
-      const authStatus = await messaging().requestPermission();
-      const enabled =
-        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-      if (enabled)
-      {
-        console.log('푸시 알림 권한 획득');
-        fetchDeviceToken();
-      }
-      else
-      {
-        Alert.alert('알림 권한이 필요합니다.');
-      }
-    };
     requestPermission();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('Foreground message received:', remoteMessage);
 
-    //알림 클릭 처리
-    messaging().onNotificationOpenedApp((remoteMessage) => {
-      console.log('알림 클릭됨', remoteMessage);
-      Alert.alert('알림 클릭', remoteMessage.notification?.title || '제목 없음');
-    });
-
-    /* // Foreground 알림 처리
-    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      console.log('Foreground Notification:', remoteMessage);
-      Alert.alert(
-        '새 알림aaa',
-        remoteMessage.notification?.title || '알림 제목 없음',
-      );
-    }); */
-
-    //backbround 알림처리
-    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      console.log('백그라운드 알림', remoteMessage);
+      // 포그라운드 상태에서 알림 표시
       await notifee.displayNotification({
-        title: remoteMessage.notification?.title || '알림 제목',
-        body: remoteMessage.notification?.body || '알림내용',
+        title: `<p style = "color: #4caf50;"><b>${remoteMessage.notification?.title}</b></p>`,
+        body: remoteMessage.notification?.body,
         android: {
-          channelId: 'default',
-          importance: AndroidImportance.HIGH,
-          smallIcon: 'ic_launcher',
+          channelId: 'update_workbook',
+        },
+      });
+      await notifee.displayNotification({
+        title: remoteMessage.notification?.title,
+        body: remoteMessage.notification?.body,
+        android: {
+          channelId: 'test',
         },
       });
     });
 
-    //종료상태 알림 처리
-    messaging().getInitialNotification().then((remoteMessage) => {
-      if(remoteMessage)
-      {
-        console.log('종료 알림 클릭', remoteMessage);
-        Alert.alert('종료상태 알림 클릭', remoteMessage.notification?.title || '제목없음');
-      }
-    });
-
-    return () => {
-      unsubscribe();
-    };
+    return unsubscribe;
+  }, []);
+  useEffect(() => {
+  // 포그라운드에서 푸시 알림을 클릭한 경우
+  notifee.onBackgroundEvent(async ({ type, detail }) => {
+    if (type === EventType.ACTION_PRESS && detail.pressAction.id === 'update_workbook') {
+      console.log('Notification clicked:', detail.notification);
+      // 푸시 알림 클릭 시 수행할 작업
+    }
+  });
   }, []);
 
+  //FCM기기토큰 설정
+  const fetchDeviceToken = async () => {
+    try
+    {
+      const deviceToken = await messaging().getToken();
+      console.log('FCM Token : ', deviceToken);
+
+      await sendTokenToServer(deviceToken);
+    }
+    catch(error)
+    {
+      console.log('FCM 토믄 생성 오류', error);
+    }
+  };
+  //FCM푸시알림권한
+  const requestPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled)
+    {
+      console.log('푸시 알림 권한 획득');
+      fetchDeviceToken();
+    }
+    else
+    {
+      Alert.alert('알림 권한이 필요합니다.');
+    }
+  };
+  // 알림 채널 생성
+  const createNotificationChannel = async () => {
+    try
+    {
+      await notifee.createChannelGroup({
+        id: 'subscribe',
+        name: 'Subscriber',
+      });
+      await notifee.createChannel({
+        id: 'update_workbook',
+        name: 'Update Monthly Workbook',
+        groupId: 'subscribe',
+        importance: AndroidImportance.DEFAULT,  // 중요도 설정
+        visibility: AndroidVisibility.PUBLIC,   //알림 보이기 설정
+      });
+    }
+    catch (error)
+    {
+      console.error('알림 채널 생성 오류', error);
+    }
+  };
   //FCM토큰 FCM서버전송 위해 Nest서버 토큰전송
   const sendTokenToServer = async (token: string) => {
     try
     {
-      const response = await axios.post('http://10.0.2.2:3000/fb/tk', {
+      await axios.post('http://10.0.2.2:3000/fb/tk', {
         token : token,
       });
-      console.log('Token sent to server:', response.data);
+      console.log('Token sent to server:');
     }
     catch (error)
     {
--- ReadingGuys/index.js
+++ ReadingGuys/index.js
@@ -6,19 +6,24 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
-import notifee from '@notifee/react-native';
 
-messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-  console.log('백그라운드 메시지 수신:', remoteMessage);
-
-  await notifee.displayNotification({
-    title: remoteMessage.notification?.title || '새로운 알림',
-    body: remoteMessage.notification?.body || '내용 없음',
-    android: {
-      channelId: 'default', // 알림 채널 ID
-      importance: notifee.AndroidImportance.HIGH, // 높은 중요도로 설정
-    },
-  });
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('백그라운드에서 메시지를 처리했습니다:', remoteMessage);
 });
+
+messaging().onNotificationOpenedApp(remoteMessage => {
+  console.log('앱이 백그라운드에서 푸시 알림을 클릭했을 때:', remoteMessage);
+  // 여기서 네비게이션 등을 통해 원하는 화면으로 이동
+});
+
+// 앱 종료 상태에서 푸시 알림 클릭 시 처리
+messaging()
+  .getInitialNotification()
+  .then(remoteMessage => {
+    if (remoteMessage) {
+      console.log('앱이 종료된 상태에서 푸시 알림을 클릭했을 때:', remoteMessage);
+      // 여기서 네비게이션 등을 통해 원하는 화면으로 이동
+    }
+  });
 
 AppRegistry.registerComponent(appName, () => App);
